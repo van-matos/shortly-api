f94b9d29d7b4ea069a7cd6ec993c946db82a07f7
van-matos <vanmatos@poli.ufrj.br>
2022-08-07 21:04:16 -0300

    Prepare for deploy

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ app.use(authRouter);
 app.use(urlRouter);
 app.use(userRouter);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
