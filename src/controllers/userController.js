72dffff4c463b19eb465abc34b221dcc22ebe710
van-matos <vanmatos@poli.ufrj.br>
2022-08-07 22:29:15 -0300

    fix: /users/me returns user info if without urls, not 404 error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,14 +8,16 @@ export async function getUser (req, res) {
             SELECT 
                 users.id as id,
                 users.name as name,
-                SUM(urls.views) as "visitCount",
-                json_agg(json_build_object(
+                COALESCE(SUM(urls."views"), 0) as "visitCount",
+                COALESCE(json_agg(json_build_object(
                     'id', urls.id,
                     'url', urls.url,
                     'views', urls.views
-                )) AS "shortenedUrls"
+                ))
+                FILTER (WHERE urls.id IS NOT NULL), '[]'::json)
+                AS "shortenedUrls"
             FROM users
-            JOIN urls
+            LEFT JOIN urls
             ON urls."userId" = users.id
             WHERE users.id = $1
             GROUP BY users.id
@@ -26,14 +28,7 @@ export async function getUser (req, res) {
             return res.sendStatus(404);
         }
 
-        const response = {
-            id: dbUsers[0].id,
-            name: dbUsers[0].name,
-            visitCount: dbUsers[0].visitCount,
-            shortenedUrls: dbUsers[0].shortenedUrls
-        };
-
-        res.send(response).status(200);
+        res.send(dbUsers);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
